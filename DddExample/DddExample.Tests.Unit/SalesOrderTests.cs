4e0ccb00082dab5cccbc9841504dfe2655dd9f7a
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-06-17 16:59:45 +0100

    Moved dddcqrsex example down one level in the tree to make room for simple ddd example.

---
+++ DddExample/DddExample.Tests.Unit/SalesOrderTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace DddExample.Tests.Unit
+{
+    [TestFixture]
+    public class SalesOrderTests
+    {
+        [Test]
+        public void InitialisingOrderShouldWork()
+        {
+            // Arrange
+            var order = new SalesOrder();
+
+            // Act
+            order.Initialise(new Id(1), new Id(2), SalesOrderStatus.New);
+
+            // Assert
+            Assert.AreEqual(1, order.Id.Value);
+            Assert.AreEqual(2, order.CustomerId.Value);
+            Assert.AreEqual(SalesOrderStatus.New, order.Status);
+        }
+    }
+}
