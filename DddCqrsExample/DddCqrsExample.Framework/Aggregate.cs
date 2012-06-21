54c398c5aebf26b4a4e19b4cbdf3cd934cdc9863
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-06-21 09:35:49 +0100

    Added presentation to root and removed unnecessary code from Aggregate base class.

--- DddCqrsExample/DddCqrsExample.Framework/Aggregate.cs
+++ DddCqrsExample/DddCqrsExample.Framework/Aggregate.cs
@@ -9,7 +9,6 @@ namespace DddCqrsExample.Framework
         private readonly IList<Event> _uncommittedEvents = new List<Event>();
 
         public string Id { get; set; }
-        public int Version { get; set; }
 
         public IEnumerable<Event> GetUncommittedEvents()
         {
@@ -21,39 +20,9 @@ namespace DddCqrsExample.Framework
             _uncommittedEvents.Clear();
         }
 
-        public void ApplyAll(IEnumerable<Event> events)
-        {
-            foreach (var evt in events)
-            {
-                InvokeEventMethod(evt);
-            }
-        }
-
         protected void Record(Event evt)
         {
             _uncommittedEvents.Add(evt);
-
-            InvokeEventMethod(evt);
-        }
-
-        private void InvokeEventMethod(Event evt)
-        {
-            var args = new object[] { evt };
-
-            var methods = GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                .Where(m => m.Name == "Apply")
-                .Where(m =>
-                {
-                    ParameterInfo[] parameters = m.GetParameters();
-                    return parameters.Length == 1 && parameters[0].ParameterType == evt.GetType();
-                });
-
-            var method = methods.FirstOrDefault();
-
-            if (method != null)
-            {
-                method.Invoke(this, args);
-            }
         }
     }
 }
